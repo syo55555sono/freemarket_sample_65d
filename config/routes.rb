a01f05a24807c72f7d14b8814b13591ad68387fe
Take-Hiro-Masa <n65.hirose.masatoshi@gmail.com>
2019-12-24 13:25:00 +0900

    トップ直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # get 'items/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root "items#index"
+  root "items#index"
   get 'registrations/index'
-  root "registrations#index"
 end
