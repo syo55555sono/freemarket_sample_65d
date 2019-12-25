75c18e58464bf958e550d00da391121d6707a390
Take-Hiro-Masa <n65.hirose.masatoshi@gmail.com>
2019-12-25 16:48:03 +0900

    signup2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # get 'items/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root "items#index"
-  get 'registrations/index'
-  root "registrations#index"
+  root "items#index"
+  resources :signup do
+    collection do
+      get "step1"
+      get "step2"
+    end
+  end
+  
+  
 end
