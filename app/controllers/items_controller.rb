a4bff10b2b12d439f1a44ef0ac95a3a2ec95957c
itoigawa <707788koo@gmail.com>
2020-01-06 18:51:04 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show]
 
   def index
-    @items = Item.all
+    @items = Item.limit(10).order('created_at DESC')
   end
 
   def new
@@ -21,6 +21,7 @@ class ItemsController < ApplicationController
 
   def show
   end
+
 
 private
 
